7a1557086b881213bd216400c352574453641816
topcuersin <ersin.topcu@useinsider.com>
2021-02-07 05:40:55 +0300

    Template uygulandı.

---
+++ resources/views/home/_welcome.blade.php
@@ -0,0 +1,35 @@
+<!-- Start About -->
+<div class="about-section-box">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 text-center">
+                <div class="inner-column">
+                    <h1>Welcome To <span>Live Dinner Restaurant</span></h1>
+                    <h4>Little Story</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor suscipit feugiat. Ut at pellentesque ante, sed convallis arcu. Nullam facilisis, eros in eleifend luctus, odio ante sodales augue, eget lacinia lectus erat et sem. </p>
+                    <p>Sed semper orci sit amet porta placerat. Etiam quis finibus eros. Sed aliquam metus lorem, a pellentesque tellus pretium a. Nulla placerat elit in justo vestibulum, et maximus sem pulvinar.</p>
+                    <a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Aydat Öde</a>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <img src="{{asset('assets')}}/images/about-img.jpg" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End About -->
+
+<!-- Start QT -->
+<div class="qt-box qt-background">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 ml-auto mr-auto text-center">
+                <p class="lead ">
+                    " Birlik ve beraberlik; ölümden başka her şeyi yener. Bir ulus, sımsıkı birbirine bağlı olmayı bildikçe yeryüzünde onu dağıtabilecek bir güç düşünülemez. Bugün vatanımızda bir milli kudret varsa, o cereyan, felaketlerden ders alan ulusun kalp ve dimağından doğmuştur. "
+                </p>
+                <span class="lead">Mustafa Kemal Atatürk</span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End QT -->
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.home')
+
+@section('title', 'Laravel Apartman Yönetim')
+@section('description')
+    Site Yönetim Sistemi
+@endsection
+
+@section('keywords','Site','Yönetim','Aidat','Hizmetler','Duyurular')
+
+
