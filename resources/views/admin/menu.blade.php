a9056d2ce9031d1c305d1236b01548c79ec593fd
topcuersin <ersin.topcu@useinsider.com>
2021-02-11 05:55:16 +0300

    Menu create and edit operations v2

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -43,7 +43,7 @@
                                             <td>{{ $rs->parent_id }}</td>
                                             <td>{{ $rs->title }}</td>
                                             <td class="center">{{ $rs->status }}</td>
-                                            <td class="center">Edit</td>
+                                            <td class="center"><a href="{{route('admin_menu_edit',['id'=>$rs->id])}}" >Edit</a></td>
                                             <td class="center"> <a href="{{route('admin_menu_delete', ['id' => $rs->id])}}" onclick="return confirm('Delete ! Are You Sure?')" >Delete</a></td>
                                         </tr>
                                         @endforeach
--- resources/views/admin/menu_edit.blade.php
+++ resources/views/admin/menu_edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 
-@section('title','Add Menu Page')
+@section('title','Edit Menu Page')
 
 @section('content')
     <!-- Page Content -->
@@ -8,12 +8,12 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Add Menu</h1>
+                    <h1 class="page-header">Edit Menu</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
             <div class="card">
-                <form class="form-horizontal" action="{{route('admin_menu_create')}}" method="post">
+                <form class="form-horizontal" action="{{route('admin_menu_update',['id'=>$data->id])}}" method="post">
                     @csrf
                     <div class="card-body">
                         <h4 class="card-title">Personal Info</h4>
@@ -23,10 +23,9 @@
                                 <select class="select2 form-control custom-select select2-hidden-accessible"
                                         style="width: 100%; height:36px;" data-select2-id="1" tabindex="-1"
                                         aria-hidden="true" name="parent_id">
-                                    <option data-select2-id="3">Main Menu</option>
+
                                     @foreach ( $datalist as $rs)
-                                        <option data-select2-id="3">Main Menu</option>
-                                        <option value="{{ $rs->id }}" data-select2-id="97">{{ $rs->title }}</option>
+                                        <option value="{{ $rs->id }}" @if ($rs->id == $data->parent_id) selected="selected" @endif>{{ $rs->title }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -34,27 +33,27 @@
                         <div class="form-group row">
                             <label for="lname" class="col-sm-1 text-right control-label col-form-label">Title</label>
                             <div class="col-sm-11">
-                                <input type="text" class="form-control" name="title" placeholder="Title">
+                                <input type="text" class="form-control" name="title" value="{{$data->title}}" placeholder="Title">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="lname" class="col-sm-1 text-right control-label col-form-label">Keywords</label>
                             <div class="col-sm-11">
-                                <input type="text" class="form-control"  name="keywords" placeholder="Keywords">
+                                <input type="text" class="form-control"  name="keywords" value="{{$data->keywords}}" placeholder="Keywords">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="lname"
                                    class="col-sm-1 text-right control-label col-form-label">Description</label>
                             <div class="col-sm-11">
-                                <input type="text" class="form-control"  name="description" placeholder="Description">
+                                <input type="text" class="form-control"  name="description" value="{{$data->description}}" placeholder="Description">
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="lname" class="col-sm-1 text-right control-label col-form-label">Slug</label>
                             <div class="col-sm-11">
-                                <input type="text" class="form-control"  name="slug" placeholder="Slug">
+                                <input type="text" class="form-control"  name="slug" value="{{$data->slug}}" placeholder="Slug">
                             </div>
                         </div>
                         <div class="form-group row" data-select2-id="95">
@@ -63,7 +62,8 @@
                                 <select class="select2 form-control custom-select select2-hidden-accessible"
                                         style="width: 100%; height:36px;" data-select2-id="1" tabindex="-1"
                                         aria-hidden="true" name="status">
-                                    <option selected="selected">False</option>
+                                    <option selected="selected">{{$data->status}}</option>
+                                    <option>False</option>
                                     <option>True</option>
                                 </select>
                             </div>
@@ -72,7 +72,7 @@
 
                         <div class="border-top">
                             <div class="card-body">
-                                <button type="submit" class="btn btn-primary">Add Menu</button>
+                                <button type="submit" class="btn btn-primary">Save</button>
                             </div>
                         </div>
                     </div>
